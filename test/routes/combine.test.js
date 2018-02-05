ae1bbc6d7b5a87bcf0cf52160573132e97f9112e
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-02-05 11:22:57 +0530

    Fixed broken tests

--- test/routes/combine.test.js
+++ test/routes/combine.test.js
@@ -3,17 +3,18 @@ const combineHandle = require('../../src/routes/combine').handle;
 describe('The handler should', () => {
   it('return a json object', (done) => {
     const promise = combineHandle();
-    promise.then((res) => {
-      const allBooks = JSON.parse(res.payload.toString()).books;
+    promise.then((allBooks) => {
       expect(allBooks.length > 0).toBeTruthy();
       done();
     });
   });
   it('return a json object with rating property', (done) => {
     const promise = combineHandle();
-    promise.then((res) => {
-      const allBooks = JSON.parse(res.payload.toString()).books;
-      expect(allBooks.length > 0).toBeTruthy();
+    promise.then((allBooks) => {
+      allBooks.map((book) => {
+        expect(book.rating).toBeTruthy();
+        return book;
+      });
       done();
     });
   });
