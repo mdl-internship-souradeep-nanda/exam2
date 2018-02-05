1294f07ca0c5fce23309bbe95e28c282b4cef1ff
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-02-05 13:55:18 +0530

    Added route tests for like and unlike

--- test/index.test.js
+++ test/index.test.js
@@ -19,4 +19,22 @@ describe('200 response should be given by API endpoint', () => {
       expect(res.statusCode).toBe(200);
     });
   });
+  it('/like/1', () => {
+    const req = {
+      method: 'GET',
+      url: '127.0.0.1:8080/like/1',
+    };
+    server.inject(req, (res) => {
+      expect(res.statusCode).toBe(200);
+    });
+  });
+  it('/unlike/1', () => {
+    const req = {
+      method: 'GET',
+      url: '127.0.0.1:8080/unlike/1',
+    };
+    server.inject(req, (res) => {
+      expect(res.statusCode).toBe(200);
+    });
+  });
 });
