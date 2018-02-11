5cfc714d2802ce7d5754cad5abe835b6067e24cc
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-02-11 21:32:21 +0530

    Refactored combine.test to take less time

--- test/routes/combine.test.js
+++ test/routes/combine.test.js
@@ -4,21 +4,22 @@ const externals = require('../../src/routes/externals');
 const { combine, group } = require('../../src/routes/combine');
 
 describe('The combine handle should', () => {
-  it('return a json object', (done) => {
+  let allBooks = [];
+  beforeAll((done) => {
     wreck.get(externals.allBooks)
-      .then(allBooks => combine(allBooks))
-      .then((allBooks) => {
-        expect(allBooks.length > 0).toBeTruthy();
+      .then(books => combine(books))
+      .then(promises => Promise.all(promises))
+      .then((books) => {
+        allBooks = books;
         done();
       });
   });
-  it('return a json object with rating property', (done) => {
-    wreck.get(externals.allBooks)
-      .then(allBooks => combine(allBooks))
-      .then((allBooks) => {
-        expect(allBooks.length > 0).toBeTruthy();
-        done();
-      });
+
+  it('return a json object with rating property', () => {
+    expect(allBooks.length).toBeGreaterThan(0);
+  });
+  it('return a json object with rating property', () => {
+    expect(allBooks[0].rating).toBeDefined();
   });
 });
 
