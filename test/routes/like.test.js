18cdbaf135a1d864afa0a9c94dca6c09fb0eddce
Souradeep Nanda <souradeep_nanda@mckinsey.com>
2018-02-11 19:19:14 +0530

    Fixed like and unlike tests

--- test/routes/like.test.js
+++ test/routes/like.test.js
@@ -2,25 +2,43 @@ const likeHandle = require('../../src/routes/like').handle;
 const models = require('../../models');
 
 describe('When like is called it should', () => {
-  beforeAll(() => {
+  beforeAll((done) => {
     models.books.destroy({ truncate: true });
+    models.books.bulkCreate([
+      {
+        id: 1,
+        name: 'name1',
+        author: 'author1',
+        rating: 5,
+        like: 0,
+      },
+      {
+        id: 2,
+        name: 'name2',
+        author: 'author2',
+        rating: 5,
+        like: 0,
+      },
+    ]).then(() => { done(); });
   });
-  it('like if not already liked', () => {
+  it('like if not already liked', (done) => {
     likeHandle(1).then(() => models.books.findOne({
       where: {
         id: 1,
       },
     })).then((book) => {
       expect(book.likes).toBe(1);
+      done();
     });
   });
-  it('stay same if liked again', () => {
+  it('stay same if liked again', (done) => {
     likeHandle(1).then(() => models.books.findOne({
       where: {
         id: 1,
       },
     })).then((book) => {
       expect(book.likes).toBe(1);
+      done();
     });
   });
 });
--- test/routes/unlike.test.js
+++ test/routes/unlike.test.js
@@ -1,26 +1,44 @@
 const unlikeHandle = require('../../src/routes/unlike').handle;
 const models = require('../../models');
 
-describe('When like is called it should', () => {
-  beforeAll(() => {
+describe('When unlike is called it should', () => {
+  beforeAll((done) => {
     models.books.destroy({ truncate: true });
+    models.books.bulkCreate([
+      {
+        id: 1,
+        name: 'name1',
+        author: 'author1',
+        rating: 5,
+        like: 1,
+      },
+      {
+        id: 2,
+        name: 'name2',
+        author: 'author2',
+        rating: 5,
+        like: 1,
+      },
+    ]).then(() => { done(); });
   });
-  it('like if not already liked', () => {
+  it('unlike the book', (done) => {
     unlikeHandle(1).then(() => models.books.findOne({
       where: {
         id: 1,
       },
     })).then((book) => {
       expect(book.likes).toBe(0);
+      done();
     });
   });
-  it('stay same if liked again', () => {
+  it('stay same if unliked again', (done) => {
     unlikeHandle(1).then(() => models.books.findOne({
       where: {
         id: 1,
       },
     })).then((book) => {
       expect(book.likes).toBe(0);
+      done();
     });
   });
 });
